b375c04984025be26683911fa62d3a9ebf184301
yungreezythatruth <57573126+yungreezythatruth@users.noreply.github.com>
2020-02-22 18:42:58 +0000

    Add files via upload

---
+++ SQL project.sql
@@ -0,0 +1,63 @@
+1)
+SELECT name
+FROM `Facilities`
+WHERE membercost =0
+
+
+2)
+SELECT COUNT( membercost ) AS free_facilities
+FROM `Facilities`
+WHERE membercost =0
+
+3)
+SELECT facid, name, membercost, monthlymaintenance
+FROM `Facilities`
+WHERE membercost < 0.2 * monthlymaintenance
+
+4)
+SELECT *
+FROM `Facilities`
+WHERE facid
+
+5)
+SELECT monthlymaintenance,
+CASE WHEN monthlymaintenance > 100 THEN 'expensive' ELSE 'cheap' END AS price_category
+FROM `Facilities`
+
+6)
+SELECT MAX( memid ) AS last_member, firstname, surname
+FROM `Members` 
+
+7)
+SELECT name AS court, 
+       firstname 
+       FROM country_club.Members members INNER JOIN country_club.Bookings bookings ON members.memid = bookings.memid INNER JOIN country_club.Facilities facilities ON facilities.facid = bookings.facid 
+WHERE name LIKE 'Tennis%' 
+AND firstname != 'GUEST'
+GROUP BY firstname 
+ORDER BY firstname 
+
+8)
+SELECT name, 
+       firstname,
+       CASE WHEN firstname = 'GUEST' THEN guestcost ELSE membercost END AS cost
+FROM country_club.Members members
+INNER JOIN country_club.Facilities facilities ON members.memid = facilities.facid INNER JOIN country_club.Bookings bookings ON bookings.facid = facilities.facid
+WHERE starttime LIKE '2012-09-14%'
+AND (3*slots) > 30
+ORDER BY 3 DESC 
+
+9)
+SELECT name, 
+       firstname,
+       CASE WHEN firstname = 'GUEST' THEN guestcost ELSE membercost END AS cost
+FROM country_club.Members members INNER JOIN country_club.Facilities facilities ON members.memid = facilities.facid INNER JOIN country_club.Bookings bookings ON bookings.facid = facilities.facid
+WHERE starttime LIKE '2012-09-14%'
+AND 3 IN (SELECT 3 
+          FROM country_club.Members members INNER JOIN country_club.Facilities facilities ON members.memid = facilities.facid INNER JOIN country_club.Bookings bookings ON bookings.facid = facilities.facid
+          WHERE (3*slots > 30))
+ORDER BY 3 DESC 
+
+10)
+
+
